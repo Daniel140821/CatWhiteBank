21865b256d9ba23f39381b2236db1e009b367481
Daniel140821 <138580504+Daniel140821@users.noreply.github.com>
2025-08-06 16:47:04 +0800

    Initial Commit

---
+++ CatWhiteBank/CatWhiteBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatWhiteBankApp.swift
+//  CatWhiteBank
+//
+//  Created by Daniel on 6/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatWhiteBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatWhiteBankTests/CatWhiteBankTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatWhiteBankTests.swift
+//  CatWhiteBankTests
+//
+//  Created by Daniel on 6/8/2025.
+//
+
+import Testing
+@testable import CatWhiteBank
+
+struct CatWhiteBankTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
