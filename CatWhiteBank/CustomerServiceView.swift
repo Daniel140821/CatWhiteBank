1b00e00b4dda59ff56c9bb98bf88ad13c8dd2767
Daniel140821 <138580504+Daniel140821@users.noreply.github.com>
2025-08-07 23:54:32 +0800

    1.1

---
+++ CatWhiteBank/CustomerServiceView.swift
@@ -0,0 +1,97 @@
+//
+//  CustomerServiceView.swift
+//  CatWhiteBank
+//
+//  Created by Daniel on 7/8/2025.
+//
+
+import SwiftUI
+
+struct CustomerServiceView: View {
+    @Environment(\.colorScheme) var colorScheme
+        
+        @State private var question: String = ""
+        
+        @State private var ChatContent : [String] = []
+        
+        var body: some View {
+            GeometryReader{Proxy in
+                ScrollView(.vertical){
+                    if ChatContent != []{
+                        VStack{
+                            
+                            ForEach(0..<ChatContent.count, id: \.self) { index in
+                                
+                                var isAI: Bool {
+                                    return ChatContent[index].contains("<aiIdentifierForAPP?>")
+                                }
+                                
+                                VStack{
+                                    
+                                    Text(removeIdentifier(str:ChatContent[index]))
+                                        .padding()
+                                        .foregroundColor(.white)
+                                        .textSelection(.enabled)
+                                        .background{
+                                            RoundedRectangle(cornerRadius: 20)
+                                                .fill(isAI ? Color(.systemGray2).gradient : Color(.blue).gradient)
+                                        }
+                                }
+                                .frame(maxWidth: .infinity, alignment: isAI ? .leading : .trailing)
+                            }
+                            
+                        }.padding()
+                    }else{
+                        VStack{
+                            Image("bank_Image")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                                .cornerRadius(.infinity)
+                                .shadow(color: colorScheme == .light ? Color(.systemGray) : .clear, radius: colorScheme == .light ? 6 : 0)
+                                .padding()
+                            
+                            Text("您好!")
+                                .font(.title.bold())
+                            Text("我是你的客戶")
+                                .font(.title.bold())
+                        }
+                        .frame(minHeight: Proxy.size.height)
+                        .frame(maxWidth: .infinity)
+                    }
+                }.animation(.easeInOut, value: ChatContent)
+            }
+            
+            Spacer()
+            
+            VStack {
+                TextField("你想問些什麼？",text: $question)
+                    .padding(.horizontal)
+                    .submitLabel(.send)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 60)
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(.infinity)
+                    .onSubmit {
+                        print(question)
+                        
+                        ChatContent.append("<userIdentifierForAPP?>\(removeIdentifier(str: question))")
+                        
+                        ChatContent.append("<aiIdentifierForAPP?>喵？")
+                        
+                        question = ""
+
+                    }
+            }
+            .padding()
+        }
+        
+        
+        private func removeIdentifier(str:String) -> String{
+            return str.replacingOccurrences(of: "<aiIdentifierForAPP?>", with: "").replacingOccurrences(of: "<userIdentifierForAPP?>", with: "")
+        }
+}
+
+#Preview {
+    CustomerServiceView()
+}
