8c41638b1c57c5aa85542c4be287f3c53b537dc8
Daniel140821 <138580504+Daniel140821@users.noreply.github.com>
2025-08-08 17:33:46 +0800

    1.3

---
+++ CatWhiteBank/PayCodeView.swift
@@ -0,0 +1,18 @@
+//
+//  PayCodeView.swift
+//  CatWhiteBank
+//
+//  Created by Daniel on 8/8/2025.
+//
+
+import SwiftUI
+
+struct PayCodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PayCodeView()
+}
---
+++ CatWhiteBank/StringToValidNumber.swift
@@ -0,0 +1,8 @@
+//
+//  StringToValidNumber.swift
+//  CatWhiteBank
+//
+//  Created by Daniel on 8/8/2025.
+//
+
+import Foundation
